676bb57d1a1bf09ab05715436f55265fac056770
Vadim Kharitonenko <vadimka.kharitonenko@mail.ru>
2023-12-30 09:56:28 +0300

    - changes for test ci/cd

--- src/pages/home/ui.vue
+++ src/pages/home/ui.vue
@@ -8,6 +8,10 @@ import { Content } from '@/shared/content';
 <template>
   <Header />
   <BottomTabNavigator />
-  <Content>CI/CD</Content>
+  <Content>
+    <div style="display: flex; height: 100vh;">
+      <img style="margin: 0 auto;" src="https://acdn.tinkoff.ru/static/pages/files/3a1b834d-6419-4ec3-9f13-d8d0dea4004b.png" />
+    </div>
+  </Content>
   <Footer />
 </template>
