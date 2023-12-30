4eabd52f3a6e2e71259302bb7d418096cd98f793
Vadim Kharitonenko <vadimka.kharitonenko@mail.ru>
2023-12-30 09:58:20 +0300

    - fix

--- src/pages/home/ui.vue
+++ src/pages/home/ui.vue
@@ -8,10 +8,6 @@ import { Content } from '@/shared/content';
 <template>
   <Header />
   <BottomTabNavigator />
-  <Content>
-    <div style="display: flex; height: 100vh;">
-      <img style="margin: 0 auto;" src="https://acdn.tinkoff.ru/static/pages/files/3a1b834d-6419-4ec3-9f13-d8d0dea4004b.png" />
-    </div>
-  </Content>
+  <Content />
   <Footer />
 </template>
